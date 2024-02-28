5f9a9542eb29318e7992a26315ab2878520513b1
brunosrd <brunosr099@outlook.com>
2024-02-28 08:40:44 -0300

    Modificacao Javascript

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-document.getElementById("encomendar").addEventListener("click", function () {
-  window.location.href = "https://github.com/brunosrd";
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("encomendar").addEventListener("click", function () {
+    window.location.href = "https://github.com/brunosrd";
+  });
 });
